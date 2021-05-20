684fb901cfa4e71f7c4cf9133df384a534436417
Dmitriy Gridchin <tigris.tstu@gmail.com>
2021-05-20 09:46:36 +0300

    Создан проект, добавлена макет фрагмента

---
+++ app/src/main/java/com/dmgpersonal/myweatherapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.dmgpersonal.myweatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dmgpersonal.myweatherapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/dmgpersonal/myweatherapp/ui/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.dmgpersonal.myweatherapp.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+    
+}
